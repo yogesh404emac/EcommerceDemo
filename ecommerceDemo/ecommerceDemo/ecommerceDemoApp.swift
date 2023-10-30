bbdbfab61af5c9e1662671ec7411da7a7352e72e
“yogesh404emac” <yogesh404emac@gmail.com>
2023-10-30 21:37:39 +0530

    Initial commit

---
+++ ecommerceDemo/ecommerceDemo/ecommerceDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ecommerceDemoApp.swift
+//  ecommerceDemo
+//
+//  Created by Yogesh on 30/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ecommerceDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
